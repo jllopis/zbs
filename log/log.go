d5494ed040d88a871850488edaa8ef45bce0a10d
Joan Llopis <jllopisg@gmail.com>
2017-03-29 18:31:49 +0200

    log: updated log with no dependencies

--- log/log.go
+++ log/log.go
@@ -1,136 +1,145 @@
-/*
-Package log is a wrapper around logrus that format an ordered slice of key/value
-pairs and logs it through logrus.
-
-The params must be of type interface{} and will be interpreted as follows:
-
-- if a message must be printed, it will be the first item of the slice
-
-- the rest of the elements will be treated as key/value pairs being the first the
-key and the second the value. They will be added to a logrus.Entry Fields
-
-- the keys mus be represented as a []byte or string and the values can be of any type
-
-Ex:
-  logI("i am a message", "somekey", "avalue", "otherkey", 23)
-
-The resulting logrus.Entry will be logged using either logrus.Entry.Info, logrus.Entry.Error,
-logrus.Entry.Fatal, logrus.Entry.Warn or logrus.Entry.Debug as for the method called.
-*/
 package log
 
 import (
 	"fmt"
+	"log"
 	"os"
+	"path"
 	"runtime"
-	"strings"
-	"time"
-
-	"github.com/Sirupsen/logrus"
 )
 
-// DefaultLogger is the logger used if no other is specified. It is a logrus.Logger
-var DefaultLogger = logrus.New()
+var (
+	defaultLogger *Log
+)
 
-type DefaultFormatter struct{}
+// StdLogger defines an interface that will accept a stdlib
+// compatible logger.
+type StdLogger interface {
+	Print(...interface{})
+	Printf(string, ...interface{})
+	Println(...interface{})
 
-func (c *DefaultFormatter) Format(entry *logrus.Entry) ([]byte, error) {
-	timestamp := time.Now().Format(time.RFC3339)
-	return []byte(fmt.Sprintf("%s %d [%s]: %s\n", timestamp, os.Getpid(), strings.ToUpper(entry.Level.String()), entry.Message)), nil
+	Fatal(...interface{})
+	Fatalf(string, ...interface{})
+	Fatalln(...interface{})
+
+	Panic(...interface{})
+	Panicf(string, ...interface{})
+	Panicln(...interface{})
 }
 
-// init initialize the DefaultLogger with some good defaults
+// Log permite la configuración del logger
+type Log struct {
+	logger  StdLogger
+	Service string
+	Version string
+}
+
+// LogConfig contiene la configuración para el logger
+type LogConfig struct {
+	Logger  *log.Logger
+	Service string
+	Version string
+}
+
 func init() {
-	DefaultLogger.Level = logrus.InfoLevel
-	DefaultLogger.Out = os.Stderr
-	DefaultLogger.Formatter = &logrus.JSONFormatter{}
-	//DefaultLogger.Formatter = &logrus.TextFormatter{DisableColors: true}
+	serviceName := os.Getenv("AD_NAME")
+	defaultLogger = &Log{
+		logger: log.New(os.Stdout,
+			"",
+			log.Ldate|log.Ltime),
+		Service: serviceName,
+		Version: "v0.0.1",
+	}
 }
 
-// SetLevel set the level of the DefaultLogger. Only messages equal or above the level
-// will be showed.
-func SetLevel(level logrus.Level) {
-	DefaultLogger.Level = level
+// New creates a new Log instance. If an error
+// occurs, *Log should be nil.
+func New(c *LogConfig) *Log {
+	return &Log{
+		logger:  c.Logger,
+		Service: c.Service,
+		Version: c.Version,
+	}
 }
 
-// LogI will log using logrus.Entry.Info
-func LogI(m ...interface{}) {
-	if len(m) == 1 {
-		DefaultLogger.Info(m...)
+// SetDefaultLogger ajusta las opciones por defecto del logger
+func SetLogger(logger StdLogger) {
+	if logger != nil {
+		defaultLogger.logger = logger
+	}
+}
+
+func (l *Log) SetService(s string) *Log {
+	if s != "" {
+		l.Service = s
+	}
+	return l
+}
+
+func (l *Log) SetVersion(v string) *Log {
+	if v != "" {
+		l.Version = v
+	}
+	return l
+}
+
+// Err publicará un log de error utilizando el formato para GCE
+// (https://cloud.google.com/error-reporting/docs/formatting-error-messages?hl=es)
+func Err(str ...interface{}) {
+	if defaultLogger.logger == nil {
 		return
 	}
-	entry, msg := prepareEntry(m)
-	entry.Info(msg)
+
+	pc, fl, ln, _ := runtime.Caller(1)
+
+	data := prepareEntry(str)
+	msg := fmt.Sprintf("svc=%s tp=error src=%s:%d fn=%s", defaultLogger.Service, path.Base(fl), ln, path.Base(runtime.FuncForPC(pc).Name()))
+	for k, v := range data {
+		if k == "msg" {
+			continue
+		}
+		msg = fmt.Sprintf("%s %s=%v", msg, k, v)
+	}
+	msg = fmt.Sprintf("%s msg=%s", msg, data["msg"])
+	defaultLogger.logger.Printf(msg)
 }
 
-// LogE will log using logrus.Entry.Error
-func LogE(m ...interface{}) {
-	if len(m) == 1 {
-		DefaultLogger.Error(m...)
+// Info publicará información de contexto, no referida a un error.
+func Info(str ...interface{}) {
+	if defaultLogger.logger == nil {
 		return
 	}
-	entry, msg := prepareEntry(m)
-	entry.Error(msg)
-}
 
-// LogF will log using logrus.Entry.Fatal
-func LogF(m ...interface{}) {
-	if len(m) == 1 {
-		DefaultLogger.Fatal(m...)
-		return
+	pc, fl, ln, _ := runtime.Caller(1)
+
+	data := prepareEntry(str)
+	msg := fmt.Sprintf("svc=%s tp=info src=%s:%d fn=%s", defaultLogger.Service, path.Base(fl), ln, path.Base(runtime.FuncForPC(pc).Name()))
+	for k, v := range data {
+		if k == "msg" {
+			continue
+		}
+		msg = fmt.Sprintf("%s %s=%v", msg, k, v)
 	}
-	entry, msg := prepareEntry(m)
-	entry.Fatal(msg)
+	msg = fmt.Sprintf("%s msg=%s", msg, data["msg"])
+	defaultLogger.logger.Printf(msg)
 }
 
-// LogD will log using logrus.Entry.Debug
-func LogD(m ...interface{}) {
-	if len(m) == 1 {
-		DefaultLogger.Debug(m...)
-		return
-	}
-	entry, msg := prepareEntry(m)
-	entry.Debug(msg)
-}
-
-// LogW will log using logrus.Entry.Warn
-func LogW(m ...interface{}) {
-	if len(m) == 1 {
-		DefaultLogger.Warn(m...)
-		return
-	}
-	entry, msg := prepareEntry(m)
-	entry.Warn(msg)
-}
-
-// LogW will log using logrus.Entry.Warn
-func LogP(m ...interface{}) {
-	if len(m) == 1 {
-		DefaultLogger.Panic(m...)
-		return
-	}
-	entry, msg := prepareEntry(m)
-	entry.Panic(msg)
-}
-
-// prepareEntry will parse the input params and build a logrus.Entry from them.
-// It will return the *logrus.Entry and the message to be printed
-func prepareEntry(m []interface{}) (*logrus.Entry, interface{}) {
-	var msg interface{}
-	data := logrus.Fields{}
+// prepareEntry will parse the input params and build a hashmap from them.
+// It will return the string with the message to log
+func prepareEntry(m []interface{}) map[string]interface{} {
+	var msg string
+	data := make(map[string]interface{})
 	if len(m)%2 != 0 {
-		msg = m[0]
+		msg = m[0].(string)
 		m = m[1:]
+	}
+	data["msg"] = msg
+	if len(m)%2 != 0 {
+		return data
 	}
 	for i := 0; i < len(m); i = i + 2 {
 		data[m[i].(string)] = m[i+1]
 	}
-	return logrus.NewEntry(DefaultLogger).WithFields(data), msg
-}
-
-func GetFunc() string {
-	pc := make([]uintptr, 2)
-	runtime.Callers(2, pc)
-	f := runtime.FuncForPC(pc[0])
-	return f.Name()
+	return data
 }
